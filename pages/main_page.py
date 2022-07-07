6d867b8d2d28425f4be618137347c455fe8afb85
Asad <102624994+Asadjon31@users.noreply.github.com>
2022-07-08 00:23:27 +0500

    Delete pages directory

--- pages/main_page.py
+++
@@ -1,7 +0,0 @@
-from .base_page import BasePage
-
-
-class MainPage(BasePage):
-    #я ваще хз что этот метод сделал тут
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
