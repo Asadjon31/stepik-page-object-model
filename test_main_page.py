3d314c5c4942433d0ac82f816d6437cecf2fb0df
Asad <102624994+Asadjon31@users.noreply.github.com>
2022-07-25 00:30:59 +0500

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,31 +1,40 @@
-from .pages.main_page import MainPage
-from .pages.base_page import BasePage
-from .pages.login_page import LoginPage
-from .pages.basket_page import BasketPage
 import pytest
+
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
+from pages.locators import MainPageLocators
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    # Гость открывает главную страницу
+    link = MainPageLocators.LINK
+    page = MainPage(browser, link)
+    page.open()
+    # Переходит в корзину по кнопке в шапке сайта
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    # Ожидаем, что в корзине нет товаров
+    basket_page.should_not_be_products()
+    # Ожидаем, что есть текст о том что корзина пуста
+    basket_page.should_be_empty_basket_message()
+
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
-   def test_guest_can_go_to_login_page(self,browser):
-       link = "http://selenium1py.pythonanywhere.com/"
-       page = MainPage(browser,
-                       link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-       page.open()  # открываем страницу
-       page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
-       login_page = LoginPage(browser, browser.current_url)
-       login_page.should_be_login_page()
+    def test_guest_can_go_to_login_page(self, browser):
+        link = MainPageLocators.LINK
+        # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page = MainPage(browser, link)
+        # открываем страницу
+        page.open()
+        # выполняем метод страницы - переходим на страницу логина
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-   def test_guest_should_see_login_link(self,browser):
-        link = "http://selenium1py.pythonanywhere.com/"
+    def test_guest_should_see_login_link(self, browser):
+        link = MainPageLocators.LINK
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
-
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_enter_basket()
-    page2 = BasketPage(browser, browser.current_url)
-    page2.is_basket_empty()
-    page2.should_be_basket_empty_message()
