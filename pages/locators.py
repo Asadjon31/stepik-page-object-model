a20ac6efc068462f1cff9380798f9143c3678e20
Asad <102624994+Asadjon31@users.noreply.github.com>
2022-07-25 00:47:08 +0500

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -4,31 +4,35 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-group .btn.btn-default")
+    BASKET_LINK = (By.XPATH,
+                   "/html/body/header/div[1]/div/div[2]/span/a")
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
+
+class BasketPageLocators():
+    BASKET_FORMSET = (By.CSS_SELECTOR, "#basket_formset")
+    EMPTY_BASKET_MESSAGE = (By.XPATH,
+                            "/html/body/div[2]/div/div[3]/div[2]/p")
+
+
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LINK = "http://selenium1py.pythonanywhere.com/"
 
 
 class LoginPageLocators():
-
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
-    REGISTER_PASSWORD_1 = (By.CSS_SELECTOR, "#id_registration-password1")
-    REGISTER_PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
-    REGISTER_BUTTON = (By.CSS_SELECTOR, "button[name='registration_submit']")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTRATION_BUTTON = (
+        By.CSS_SELECTOR, "#register_form button[name=registration_submit]")
 
 
 class ProductPageLocators():
-    BUTTON_PRODUCT = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
-    BOOK_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
-    BOOK_PRICE_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(3) strong")
-    BOOK_NAME_BASKET = (By.CSS_SELECTOR, "#messages>div:nth-child(1) strong")
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>div:nth-child(1)")
-
-class BasketPageLocators():
-    EMPTY_BASKET = (By.CSS_SELECTOR, ".basket-items")
-    EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main>.price_color')
+    PRODUCT_TITLE = (By.CSS_SELECTOR, 'div.product_main>h1')
+    MESSAGE_PRODUCT_TITLE = (By.CSS_SELECTOR, '#messages .alertinner>strong')
+    MESSAGE_COST_BASKET = (By.CSS_SELECTOR, '#messages .alertinner>p>strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages .alert-success')
