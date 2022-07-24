3db3b7d0999f03317d28d1d3c654097621d97d72
Asad <102624994+Asadjon31@users.noreply.github.com>
2022-07-25 00:51:49 +0500

    Update product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,26 +1,49 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from .locators import ProductPageLocators
+
+
 class ProductPage(BasePage):
-    def add_to_basket(self):
-        assert self.is_element_present(*ProductPageLocators.BUTTON_PRODUCT), "Button is not presented"
-        button = self.browser.find_element(*ProductPageLocators.BUTTON_PRODUCT)
-        button.click()
 
-    def should_basket_price(self):
-        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
-        basket_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_BASKET).text
-        assert book_price == basket_price, "Price is not same"
+    def add_product_to_basket(self):
+        btn = self.browser.find_element(
+            *ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        btn.click()
 
-    def should_be_book_name(self):
-        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
-        book_basket = self.browser.find_element(*ProductPageLocators.BOOK_NAME_BASKET).text
-        assert book_name == book_basket, "Name is not same"
+    def get_product_title(self):
+        element = self.browser.find_element(
+            *ProductPageLocators.PRODUCT_TITLE)
+        return element.text
+
+    def get_product_price(self):
+        element = self.browser.find_element(
+            *ProductPageLocators.PRODUCT_PRICE)
+        return element.text
+
+    def get_message_product_title(self):
+        element = self.browser.find_element(
+            *ProductPageLocators.MESSAGE_PRODUCT_TITLE)
+        return element.text
+
+    def get_message_cost_basket(self):
+        element = self.browser.find_element(
+            *ProductPageLocators.MESSAGE_COST_BASKET)
+        return element.text
+
+    def check_message_product_has_been_added_to_basket(self):
+        title = self.get_product_title()
+        title_from_message = self.get_message_product_title()
+        assert title == title_from_message, \
+            f'Expected "{title}", but "{title_from_message}" was received'
+
+    def check_message_cost_of_the_basket(self):
+        price = self.get_product_price()
+        cost_basket_from_message = self.get_message_cost_basket()
+        assert price == cost_basket_from_message, f'Expected "{price}", but "{cost_basket_from_message}" was received'
 
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
 
-    def should_dissapear_of_success_message(self):
+    def should_be_disappeared_success_message(self):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
-            "Success message is presented, but should not be"
+            "Success message is not disappeared, but should was"
