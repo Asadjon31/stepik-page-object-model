de222cc1cb6e1a90570954f45d95a14bc032348a
Asad <102624994+Asadjon31@users.noreply.github.com>
2022-07-25 00:45:50 +0500

    Update basket_page.py

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,11 +1,12 @@
 from .base_page import BasePage
 from .locators import BasketPageLocators
-from selenium.webdriver.common.by import By
 
 
 class BasketPage(BasePage):
-    def should_be_basket_empty_message(self):
-        assert self.is_not_element_present(*BasketPageLocators.EMPTY_BASKET), "Item in basket"
+    def should_not_be_products(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_FORMSET), \
+            "Products is presented, but should not be"
 
-    def is_basket_empty(self):
-        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "Basket is not empty"
+    def should_be_empty_basket_message(self):
+        assert self.is_element_present(
+            *BasketPageLocators.EMPTY_BASKET_MESSAGE), "Empty basket message is not presented"
