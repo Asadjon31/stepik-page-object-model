fd88725e95f7ddeeafd8cf15b616cc492ef52dc1
Asad <102624994+Asadjon31@users.noreply.github.com>
2022-07-25 00:48:10 +0500

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,28 +3,36 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        self.should_be_register_form()
+        email_field = self.browser.find_element(
+            *LoginPageLocators.REGISTRATION_EMAIL)
+        email_field.send_keys(email)
+        password1_field = self.browser.find_element(
+            *LoginPageLocators.REGISTRATION_PASSWORD1)
+        password1_field.send_keys(password)
+        password2_field = self.browser.find_element(
+            *LoginPageLocators.REGISTRATION_PASSWORD2)
+        password2_field.send_keys(password)
+        password1_field = self.browser.find_element(
+            *LoginPageLocators.REGISTRATION_BUTTON)
+        password1_field.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.browser.current_url, "There is not login in url"
-
+        # реализована проверка на корректный url адрес
+        assert "login" in self.browser.current_url, "This is not a login URL"
 
     def should_be_login_form(self):
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        # реализована проверка, что есть форма логина
+        assert self.is_element_present(
+            *LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
-
-
-    def register_new_user(self, email, password):
-        email_field = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
-        email_field.send_keys(email)
-        password_field1 = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD_1)
-        password_field1.send_keys(password)
-        password_field2 = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD_2)
-        password_field2.send_keys(password)
-        button_submit = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
-        button_submit.click()
+        # реализована проверка, что есть форма регистрации на странице
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTER_FORM), "Register form is not presented"
